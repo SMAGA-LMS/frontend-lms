55e4cf884fb68307c822662ab6ee9efcdc8c538a
Syauqi <syauqifrizman@gmail.com>
2024-07-16 15:14:07 +0700

    feat: Update dependencies, add react-router-dom, add animation redirect to LoginScreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import SplashScreen from "./pages/onboarding/SplashScreen";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import LoginScreen from "./pages/login/LoginScreen";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+      navigate("/login");
+    }, 4000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [navigate]);
+
   return (
     <>
-      <SplashScreen />
+      {loading ? (
+        <SplashScreen />
+      ) : (
+        <Routes>
+          <Route path="/login" element={<LoginScreen />} />
+        </Routes>
+      )}
     </>
   );
 }
