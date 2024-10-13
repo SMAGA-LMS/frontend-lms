90ded1f62ccb1ae4edac599c560bedc2ce1ca949
Syauqi <syauqifrizman@gmail.com>
2024-10-13 16:54:10 +0700

    fix: change splashscreen logo

--- src/pages/SplashScreen.tsx
+++ src/pages/SplashScreen.tsx
@@ -1,5 +1,4 @@
-import ThumbnailWithFooter from "@/components/global/ThumbnailWithFooter";
-import thumbnailStudents from "../assets/images/thumbnail-students.svg";
+import LMSkuLogo from "@/assets/images/LMSku-logo-latest.png";
 import { useEffect, useState } from "react";
 
 export default function SplashScreen() {
@@ -9,7 +8,7 @@ export default function SplashScreen() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setAnimationClass("animate-fadeOut");
-    }, 1000); // 1500ms default
+    }, 1000); // 1000ms default
 
     return () => clearTimeout(timer);
   }, []);
@@ -18,12 +17,12 @@ export default function SplashScreen() {
       className={`bg-white flex items-center justify-center ${animationClass}`}
     >
       <div className="w-full h-screen">
-        <ThumbnailWithFooter
-          image={thumbnailStudents}
-          footerText="Welcome!"
-          isHScreen="h-screen"
-          sizeText="text-base"
-        />
+        <div className="flex flex-col items-center justify-center h-screen mx-4">
+          <img src={LMSkuLogo} alt="LMSku Logo" className="w-auto h-48 mt-2" />
+          <p className="text-lg font-semibold bg-smagaLMS-soft-white p-2 rounded-lg">
+            Learning Management System
+          </p>
+        </div>
       </div>
     </div>
   );
